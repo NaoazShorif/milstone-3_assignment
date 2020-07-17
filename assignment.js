7cea8078c004e58288e3e6777710a4660ac8268f
naoaz shorif <naoazshorif420@gamil.com>
2020-07-17 17:27:41 +0600

    done 3 problems

---
+++ assignment.js
@@ -0,0 +1,60 @@
+//1st problem: calculating feet to Mile.
+function feetToMile( feet ){
+    let mile = feet / 5280;
+    if (feet >= 0){
+      return"Your converted result is " + mile.toFixed(4) + " mile.";
+    }else{
+      return "WARNING: Please enter a valid or positive value.";
+    }
+}
+//testing the feetToMile function.
+let resultInMile = feetToMile(120);
+console.log (resultInMile);
+
+//2nd problem: calculating total amount of wood.
+function woodCalculator(chair, table , bed){
+    let calculateWood = [chair* 1, table * 3, bed * 5]
+    let totalNeedOfWood = calculateWood[0]+ calculateWood[1] + calculateWood[2];
+    if (chair < 0 || table < 0 ||bed < 0){
+        return "WARNING: Please enter a valid or positive number of your furniture";
+    }else {
+        return "You need to buy " + totalNeedOfWood + " cubic feet wood.";
+    }
+}
+//testing the woodCalculator function.
+let totalWoodResult = woodCalculator(1, 1, 1);
+console.log(totalWoodResult);
+
+//3rd problem: Calculating brick for your building.
+// function brickCalculator (tala){
+//     if( tala < 0){
+//         return "WARNING: Building can't contain negetive value.";
+//     }else if (tala >= 0 && tala <= 10){
+//         let bricks = tala * 15 * 1000;
+//         return bricks;
+//     }else if ()
+// }
+
+// console.log(brickCalculator (10));
+
+
+
+//4th problem: Finding tiny friends.
+function tinyFriend (names){
+    let tinyLength = names[0].length;
+    let tinyName = "";
+
+        for (i = 0; i < names.length; i++){
+            let currentName = names[i].length;
+            if (currentName < tinyLength){
+                tinyName = names[i];
+                tinyLength = currentName;
+            }
+        }
+        return tinyName; 
+}
+
+// testing 4th problem or tinyFriend function;
+let friends = ["rahim", "jabbar","picchi", "abul","sabbir"];
+let result = tinyFriend(friends);
+console.log(result);
